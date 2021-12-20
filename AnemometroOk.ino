ff041b519fb12364873f1cf9a8facb75eca9a00f
delaineS <92988734+delaineS@users.noreply.github.com>
2021-12-20 14:57:55 +0100

    Update AnemometroOk.ino

--- AnemometroOk.ino
+++ AnemometroOk.ino
@@ -1,7 +1,7 @@
 const byte   interruptPin = 14; // Or other pins that support an interrupt
 unsigned int Debounce_Timer, Current_Event_Time, Last_Event_Time, Event_Counter, RPS;
 float        WindSpeed;
-const float r = 7.25;
+const float r = 2.733018;
 
 void setup() {
   Serial.begin(115200);
@@ -32,9 +32,8 @@ ICACHE_RAM_ATTR void WSpeed_ISR (void) {
 }
 
 void Timer_ISR (void) {                                                       // Timer reached zero, now re-load it to repeat
-  timer0_write(ESP.getCycleCount() + 80000000L);    // Reset the timer, do this first for timing accuracy
-  RPS = (3/25) * 3.1414 * r;
-  WindSpeed = (Event_Counter * RPS);
+  timer0_write(ESP.getCycleCount() + 80000000L); // Reset the timer, do this first for timing accuracy
+  WindSpeed = (Event_Counter*r);
   //Serial.println(Event_Counter);
   Event_Counter =0;
 }
